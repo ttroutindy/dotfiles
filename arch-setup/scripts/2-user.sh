78c9011ef25391b2a41abc8e9ee9f4ce3b8c94f3
Travis Trout <ttroutindy@gmail.com>
2022-04-28 22:45:54 -0400

    initial update of Auto Arch

---
+++ arch-setup/scripts/2-user.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#github-action genshdoc
+echo -ne "
+-------------------------------------------------------------------------
+ █████╗ ██╗   ██╗████████╗ ██████╗        █████╗ ██████╗  ██████╗██╗  ██╗
+██╔══██╗██║   ██║╚══██╔══╝██╔═══██╗      ██╔══██╗██╔══██╗██╔════╝██║  ██║
+███████║██║   ██║   ██║   ██║   ██║█████╗███████║██████╔╝██║     ███████║
+██╔══██║██║   ██║   ██║   ██║   ██║╚════╝██╔══██║██╔══██╗██║     ██╔══██║
+██║  ██║╚██████╔╝   ██║   ╚██████╔╝      ██║  ██║██║  ██║╚██████╗██║  ██║
+╚═╝  ╚═╝ ╚═════╝    ╚═╝    ╚═════╝       ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                        SCRIPTHOME: arch-setup
+-------------------------------------------------------------------------
+
+Installing AUR Softwares
+"
+source $HOME/arch-setup/configs/setup.conf
+
+sed -n '/'$INSTALL_TYPE'/q;p' ~/arch-setup/pkg-files/${DESKTOP_ENV}.txt | while read line
+do
+  if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]
+  then
+    # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+    continue
+  fi
+  echo "INSTALLING: ${line}"
+  sudo pacman -S --noconfirm --needed ${line}
+done
+
+
+if [[ ! $AUR_HELPER == none ]]; then
+  cd ~
+  git clone "https://aur.archlinux.org/$AUR_HELPER.git"
+  cd ~/$AUR_HELPER
+  makepkg -si --noconfirm
+  # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
+  # stop the script and move on, not installing any more packages below that line
+  sed -n '/'$INSTALL_TYPE'/q;p' ~/arch-setup/pkg-files/aur-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    $AUR_HELPER -S --noconfirm --needed ${line}
+  done
+fi
+
+export PATH=$PATH:~/.local/bin
+
+echo -ne "
+-------------------------------------------------------------------------
+                    SYSTEM READY FOR 3-post-setup.sh
+-------------------------------------------------------------------------
+"
+exit
---
+++ config/qtile/autostart.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+#/usr/bin/emacs --daemon &
+nitrogen --restore &
+# picom --config  $HOME/.config/picom/picom.conf &
+volumeicon &
+steam &
